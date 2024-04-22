e5988da6dd841a86614abb315fb9b0622fc783c4
RitankS <146180073+RitankS@users.noreply.github.com>
2024-04-22 15:35:09 +0530

    ngrok

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-import express from "express"
-import cors from "cors"
-import stripe from "./router/router.js"
-import bodyparser from 'body-parser'
-const app = express()
-const port = 2001
+import express from "express";
+import cors from "cors";
+import stripe from "./router/router.js";
+import bodyparser from 'body-parser';
+import ngrok from "ngrok";
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyparser.json())
-app.use("/pay" , stripe)
 
-app.listen(port, ()=>{
-    console.log("App is listening fine")
-})
+const app = express();
+const port = 2001;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyparser.json());
+app.use("/pay", stripe);
+
+
+app.listen(port, () => {
+    console.log("App is listening fine");
+
+
+   ngrok.connect({ addr: port, authtoken: process.env.NGROK_AUTHTOKEN })
+        .then(ngrokUrl => {
+            console.log(`ngrok in: ${ngrokUrl}`);
+        })
+        .catch(error => {
+            console.log({ error: error.message });
+        });
+});
--- router/router.js
+++ router/router.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { mobeAllZero,getTheInteger , startStripeFlow , takePrice , createSession , sendEmail , monthlySubs , getTheWebHookPayLoad , testingPublicAPI} from "../routes/routes.js"
+import { getTheInteger , startStripeFlow , takePrice , createSession , sendEmail , monthlySubs , getTheWebHookPayLoad , testingPublicAPI} from "../routes/routes.js"
 const router = express.Router()
 
 
@@ -11,7 +11,7 @@ router.post("/email" , sendEmail)
 router.post("/subs" , monthlySubs)
 router.post("/getpayload" , getTheWebHookPayLoad)
 router.get("/test" , testingPublicAPI)
-router.get('/zero' , mobeAllZero)
+
 
 
 
